866fa429ec883a2cfeaf79b4a31b92b4d4321324
EissenLu <lukas.eissen@yahoo.de>
2022-11-22 14:32:00 +0100

    Beispiele für Intercepts

--- cypress/e2e/intercept.cy.js
+++ cypress/e2e/intercept.cy.js
@@ -1,6 +1,19 @@
-describe('First test', () => {
+describe('Intercept', () => {
     
     it('My First test', () =>{
         cy.visit('')
+
+        cy.intercept('GET', '/api-url',
+            { fixture: 'intercept.json'})
+            .as('example')
+        
+        cy.intercept('/api-url',
+        {
+            statusCode: 404,
+            body: null,
+            headers: {
+                'x-not-found': 'true'
+            }
+        }).as('not-available')
     })
 })
